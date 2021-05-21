19325bd902e359b30d2d1bd1323ae779d39f03af
Magnus Ihse Bursie <mag@icus.se>
2021-05-21 15:02:03 +0200

    Clean up json files

--- src/main/java/se/icus/mag/statuseffecttimer/mixin/StatusEffectTimerMixin.java
+++ src/main/java/se/icus/mag/statuseffecttimer/mixin/StatusEffectTimerMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class StatusEffectTimerMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
 		System.out.println("This line is printed by an example mod mixin!");
